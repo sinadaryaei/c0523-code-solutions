7bec57a222e675dab7f96610f5b085a72121f436
sinadaryaei <sinatangssiran@gmail.com>
2023-05-08 23:03:15 +0000

    Completed javascript-operators-and-expressions

---
+++ javascript-operators-and-expressions/main.js
@@ -0,0 +1,50 @@
+const width = 2;
+const height = 4;
+
+const area = width * height;
+
+console.log('the value of area:', area);
+console.log('the typeof area:', typeof area);
+
+const bill = 10;
+const payment = 20;
+
+const change = payment - bill;
+
+console.log('the value of change:', change);
+console.log('the type of change:', typeof change);
+
+const quizzes = 25;
+const midterms = 50;
+const finals = 75;
+const grade = ((quizzes + midterms + finals) / 3);
+
+console.log('the value of grade:', grade);
+console.log('the type of grade:', typeof grade);
+
+const firstName = 'Ron';
+const lastName = 'Swanson';
+
+let fullName = firstName + ' ' + lastName;
+
+console.log('the value of fullName:', fullName);
+console.log('the typeof of fullName:', typeof fullName);
+
+const pH = 10;
+const isAcidic = pH < 7;
+
+console.log('the value of isAcidic:', isAcidic);
+console.log('the typeof isAcidic', typeof isAcidic);
+
+const headCount = 1;
+const isSparta = headCount === 300;
+
+console.log('the value of isSparta:', isSparta);
+console.log('the typeof isSparta:', typeof isSparta);
+
+
+let motto = fullName;
+motto += ' is the Goat';
+
+console.log('the value of motto:', motto);
+console.log('the typeof motto:', typeof motto);

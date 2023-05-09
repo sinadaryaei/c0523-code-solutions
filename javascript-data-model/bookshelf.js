9643c57405b046c9902164184f6cd1b1bd34dc5b
sinadaryaei <sinatangssiran@gmail.com>
2023-05-09 18:50:11 +0000

    Accessing Nested Data in javascript-data-model

---
+++ javascript-data-model/bookshelf.js
@@ -0,0 +1,28 @@
+var bookshelf = [
+  {
+    isbn: '978-1491929483',
+    title:
+      'Introduction to JavaScript Object Notation: A To-the-Point Guide to JSON',
+    author: 'Lindsay Bassett',
+  },
+  {
+    isbn: '978-1484218624',
+    title: 'JSON Quick Syntax Reference',
+    author: 'Wallace Jackson',
+  },
+  {
+    isbn: '978-0692232699',
+    title:
+      "Build APIs You Won't Hate: Everyone and their dog wants an API, so you should probably learn how to build them",
+    author: 'Phil Sturgeon & Laura Bohill',
+  },
+];
+
+const secondAuthorName = bookshelf[1].author;
+console.log('value of secondAuthorName:', secondAuthorName);
+
+const isbnFirstBook = bookshelf[0].isbn;
+console.log('value of isnbFirstBook:', isbnFirstBook);
+
+const titleThirdBook = bookshelf[2].title;
+console.log('value of titleThirdBook', titleThirdBook);

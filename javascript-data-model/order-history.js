8bf236e9895003f1bf95a32152f5b7d3029213d9
sinadaryaei <sinatangssiran@gmail.com>
2023-05-09 21:41:41 +0000

    Updated structure of the order-history.js in javascript-data-model

--- javascript-data-model/order-history.js
+++ javascript-data-model/order-history.js
@@ -5,10 +5,15 @@ const orderHistory = [
     total: 34.0,
     orderPlaced: 'August 4, 2020',
     delivered: 'Aug 8, 2020',
-    returnWindow: 'Sep 7, 2020',
-    book: 'Javasript for imatient programmers',
-    author: 'Rauchmayer, Dr. Axel',
-    price: 31.55,
+    items: [
+      {
+        type: 'book',
+        title: 'Javasript for imatient programmers',
+        author: 'Rauchmayer, Dr. Axel',
+        returnWindow: 'Sep 7, 2020',
+        price: 31.55,
+      },
+    ],
   },
   {
     orderNumber: '113-9984268-1280257',
@@ -16,10 +21,15 @@ const orderHistory = [
     total: 44.53,
     orderPlaced: 'July 19, 2020',
     delivered: 'July 20, 2020',
-    returnWindow: 'Aug 19, 2020',
-    book: 'The Timeless Way of BUilding',
-    author: 'Alexander, Christopher',
-    price: 41.33,
+    items: [
+      {
+        type: 'book',
+        title: 'The Timeless Way of BUilding',
+        author: 'Alexander, Christopher',
+        returnWindow: 'Aug 19, 2020',
+        price: 41.33,
+      },
+    ],
   },
   {
     orderNumber: '114-28755557-9059409',
@@ -27,9 +37,14 @@ const orderHistory = [
     total: 17.22,
     orderPlaced: 'July 4, 2020',
     delivered: 'Jul 7, 2020',
-    returnWindow: 'Aug 5, 2020',
-    item: 'Gamecube controller Adapter.',
-    price: 15.99,
+    items: [
+      {
+        type: 'Hardware',
+        title: 'Gamecube controller Adapter.',
+        returnWindow: 'Aug 5, 2020',
+        price: 15.99,
+      },
+    ],
   },
   {
     orderNumber: '113-2883177-2648248',
@@ -37,20 +52,23 @@ const orderHistory = [
     total: 138.93,
     orderPlaced: 'July 5, 2020',
     delivered: 'Aug 4, 2020',
-    returnWindow: 'Aug 4, 2020',
-    item1: [
+    items: [
       {
-        item: 'Gamecube Controller - Super Smash Bros. Edition (Nintendo Switch)',
+        type: 'Hardware',
+        title:
+          'Gamecube Controller - Super Smash Bros. Edition (Nintendo Switch)',
+        returnWindow: 'Aug 4, 2020',
         price: 94.95,
       },
-    ],
-    item2: [
       {
-        book: 'The art of Sql',
+        type: 'Book',
+        title: 'The art of Sql',
         author: 'Faroult, Stephane',
+        returnWindow: 'Aug 4, 2020',
         price: 15.99,
       },
     ],
   },
 ];
+
 console.log('value of orderHistory:', orderHistory);

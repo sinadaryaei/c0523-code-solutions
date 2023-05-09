99e2f2b8d35bb38d2b70c9927cda6f318f1a146a
sinadaryaei <sinatangssiran@gmail.com>
2023-05-09 23:23:50 +0000

    Completed exercises javascript-functions

--- javascript-functions/main.js
+++ javascript-functions/main.js
@@ -2,16 +2,33 @@ function convertMinutesToSeconds(minutes) {
   const numberOfMinutes = 60 * minutes;
   return numberOfMinutes;
 }
-console.log('value of numberOfMinutes:', convertMinutesToSeconds(5));
+console.log('value of convertMinutesToSeconds:', convertMinutesToSeconds(5));
 
 function greet(name) {
   const personName = 'Hey, ' + name;
   return personName;
 }
-console.log('value of personName:', greet('Beavis'));
+console.log('value of greet:', greet('Beavis'));
 
 function getArea(width, height) {
   const areaOfRectangle = width * height;
   return areaOfRectangle;
 }
-console.log('value of areaOfRectangle:', getArea(17, 42));
+console.log('value of getArea:', getArea(17, 42));
+
+function getFirstName(person) {
+  const names = [
+    {
+      firstName: 'Lelouche',
+      lastName: 'Lamperouge',
+    },
+  ];
+  return names[0].firstName;
+}
+console.log('value of getFirstName:', getFirstName());
+
+function getLastElement(array) {
+  const myArray = ['propane', 'and', 'propane', 'accessories'];
+  return myArray[myArray.length - 1];
+}
+console.log('value of getLastElement', getLastElement());

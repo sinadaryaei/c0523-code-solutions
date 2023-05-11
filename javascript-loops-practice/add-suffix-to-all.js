2f2076485525f0e66ec56eee3c90ce5e690973b6
sinadaryaei <sinatangssiran@gmail.com>
2023-05-11 22:05:17 +0000

    completed step 4 in javascript-loops-practice.

--- javascript-loops-practice/add-suffix-to-all.js
+++ javascript-loops-practice/add-suffix-to-all.js
@@ -1,1 +1,9 @@
 /* exported addSuffixToAll */
+function addSuffixToAll(words, suffix) {
+  const newArray = [];
+  for (let i = 0; i < words.length; i++) {
+    const wordSuffix = words[i] + suffix;
+    newArray.push(wordSuffix);
+  }
+  return newArray;
+}

38fe774b051030f6f48cce8c95644684775d3197
sinadaryaei <sinatangssiran@gmail.com>
2023-05-12 17:10:24 +0000

    Completed step 6 of string-manipulation-1 exercise.

--- string-manipulation-1/src/is-vowel.js
+++ string-manipulation-1/src/is-vowel.js
@@ -1,1 +1,14 @@
 /* exported isVowel */
+function isVowel(char) {
+  switch (char.toLowerCase()) {
+    case 'a':
+    case 'e':
+    case 'i':
+    case 'o':
+    case 'u':
+    case 'y':
+      return true;
+    default:
+      return false;
+  }
+}

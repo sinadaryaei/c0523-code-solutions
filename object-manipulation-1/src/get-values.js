4968e5d6cd61b8c6c06ab7656071910e6d0d8a7b
sinadaryaei <sinatangssiran@gmail.com>
2023-05-17 20:39:14 +0000

    Completed Step 4. getValues(object) in object-manipulation-1 exercise.

--- object-manipulation-1/src/get-values.js
+++ object-manipulation-1/src/get-values.js
@@ -1,4 +1,8 @@
 /* exported getValues */
-function getValues(object, key) {
-  return object[key];
+function getValues(object) {
+  const arrayValue = [];
+  for (const property in object) {
+    arrayValue.push(object[property]);
+  }
+  return arrayValue;
 }

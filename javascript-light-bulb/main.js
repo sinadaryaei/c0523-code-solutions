e912af261a8450e2b2f42b456ced238e49be9183
sinadaryaei <sinatangssiran@gmail.com>
2023-05-23 22:01:35 +0000

    Completed javascript-light-bulb exercise.

--- javascript-light-bulb/main.js
+++ javascript-light-bulb/main.js
@@ -1,7 +1,15 @@
 const $light = document.querySelector('.light');
 const $background = document.querySelector('.background');
+let $lightOn = true;
 
 $light.addEventListener('click', () => {
-  $light.className = '.light.on';
-  $background.className = '.background.on';
+  if ($lightOn) {
+    $light.className = 'light on';
+    $background.className = 'background on';
+    $lightOn = false;
+  } else {
+    $light.className = 'light';
+    $background.className = 'background';
+    $lightOn = true;
+  }
 });

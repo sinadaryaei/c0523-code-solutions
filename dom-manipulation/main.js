89bb93cc396bc6822eb5783eb8f8b3df44e59ac2
sinadaryaei <sinatangssiran@gmail.com>
2023-05-23 03:52:14 +0000

    Added the className button now changes when reaches number of clicks | dom-manipulation exercise.

--- dom-manipulation/main.js
+++ dom-manipulation/main.js
@@ -7,4 +7,17 @@ $hotButton.addEventListener('click', (event) => {
   console.log(event);
   console.log(event.target);
   $clickCount.textContent = numberOfClicks;
+  if (numberOfClicks < 4) {
+    $hotButton.className = 'hot-button cold';
+  } else if (numberOfClicks < 7) {
+    $hotButton.className = 'hot-button cool';
+  } else if (numberOfClicks < 10) {
+    $hotButton.className = 'hot-button tepid';
+  } else if (numberOfClicks < 13) {
+    $hotButton.className = 'hot-button warm';
+  } else if (numberOfClicks < 16) {
+    $hotButton.className = 'hot-button hot';
+  } else {
+    $hotButton.className = 'hot-button nuclear';
+  }
 });

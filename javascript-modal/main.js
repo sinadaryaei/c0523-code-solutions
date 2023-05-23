0981c77afabf945be3e679d744bdffc4303c69db
sinadaryaei <sinatangssiran@gmail.com>
2023-05-23 23:52:05 +0000

    Adding main.js functionality javascript-modal exercise.

---
+++ javascript-modal/main.js
@@ -0,0 +1,12 @@
+// const $openModal = document.querySelector('#open-btn');
+// const $noButton = document.querySelector('#no-btn');
+// const $hiddenModal = document.getElementById('box-modal');
+// const $openClicked = false;
+
+// $openModal.addEventListener('click', ()=> {
+//   if ($openClicked) {
+//     $hiddenModal.classList.add('box-modal');
+//   }
+//   console.log('event.target', event.target);
+// });
+// console.log('event:', event);

5847ca82dd1af65cd4272a137a403f4986ab57ad
sinadaryaei <sinatangssiran@gmail.com>
2023-05-24 19:00:41 +0000

    Completed javascript-form exercise.

--- javascript-forms/form-values.js
+++ javascript-forms/form-values.js
@@ -4,8 +4,8 @@ $form.addEventListener('submit', (event) => {
   event.preventDefault();
   const formData = {
     name: $form.elements.name.value,
-    email: $form.elements.name.value,
-    message: $form.elements.name.value,
+    email: $form.elements.email.value,
+    message: $form.elements.message.value,
   };
   console.log('formData:', formData);
   $form.reset();

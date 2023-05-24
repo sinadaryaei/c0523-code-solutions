a47a4d3a58350491d58952d597235230c87320e9
sinadaryaei <sinatangssiran@gmail.com>
2023-05-24 18:05:02 +0000

    Step 13. in javascript-froms exercise completed.

--- javascript-forms/form-values.js
+++ javascript-forms/form-values.js
@@ -2,12 +2,10 @@ const $form = document.querySelector('#contact-form');
 
 $form.addEventListener('submit', (event) => {
   event.preventDefault();
+  const formData = {
+    name: $form.elements.name.value,
+    email: $form.elements.name.value,
+    message: $form.elements.name.value,
+  };
+  console.log('formData:', formData);
 });
-
-const formData = {
-  nameValue: $form.event.name.value,
-  emailValue: $form.event.email.value,
-  messageValue: $form.event.message.value,
-};
-
-console.log(formData);

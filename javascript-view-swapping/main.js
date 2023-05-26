4f3c609ca0b113f9c0227a876fccfb514a2d30bd
sinadaryaei <sinatangssiran@gmail.com>
2023-05-26 01:52:55 +0000

    Step 6.(ii) in javascript-view-swapping exercise complete.

--- javascript-view-swapping/main.js
+++ javascript-view-swapping/main.js
@@ -1,26 +1,28 @@
-// const $tabContainer = document.querySelector('.tab-container');
-// const $allTab = document.querySelectorAll('.tab');
-// const $allView = document.querySelectorAll('.view');
+const $tabContainer = document.querySelector('.tab-container');
+const $allTab = document.querySelectorAll('.tab');
+const $allView = document.querySelectorAll('.view');
 
-// $tabContainer.addEventListener('click', (event) => {
-//   if (event.target.matches('.tab')) {
-//     for (let i = 0; i < $allTab.length; i++) {
-//       const loopTab = $allTab[i];
+$tabContainer.addEventListener('click', (event) => {
+  if (event.target.matches('.tab')) {
+    for (let i = 0; i < $allTab.length; i++) {
+      const loopTab = $allTab[i];
 
-//       if (loopTab === event.target) {
-//         $allTab[i].classList.add('active');
-//       } else {
-//         $allTab[i].classList.remove('active');
-//       }
-//     }
-//   }
-//     const $dataView = event.target.getAttribute('data-view');
-//     for (let i = 0; i < $allView.length; i++) {
-//       const loopView = $allView[i];
-//       if ($allView[i] === $dataView) {
-//         $allView[i].classList.add('hidden');
-//       } else {
-//         $allView[i].classList.remove('hidden');
-//       }
-//     }
-// });
+      if (loopTab === event.target) {
+        $allTab[i].classList.add('active');
+      } else {
+        $allTab[i].classList.remove('active');
+      }
+    }
+    const $dataView = event.target.getAttribute('data-view');
+    console.log($dataView);
+    for (let i = 0; i < $allView.length; i++) {
+      const loopView = $allView[i];
+      console.log('loopView', loopView);
+      if (loopView.getAttribute('data-view') === $dataView) {
+        $allView[i].classList.remove('hidden');
+      } else {
+        $allView[i].classList.add('hidden');
+      }
+    }
+  }
+});

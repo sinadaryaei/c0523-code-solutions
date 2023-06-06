d9066426c3a26a9c2d5dbfe4fcd0e1fb1e948f80
sinadaryaei <sinatangssiran@gmail.com>
2023-06-06 20:46:05 +0000

    Completed new-objects.js | javascript-new.

---
+++ javascript-new/new-objects.js
@@ -0,0 +1,14 @@
+const anObject = new Object({});
+
+console.log('value of anObject:', anObject);
+console.log('typeof of anObject:', typeof anObject);
+
+const anArray = new Array();
+
+console.log('value of anArray:', anArray);
+console.log('typeof of anArray:', typeof anArray);
+
+const aFunction = new Function();
+
+console.log('value of aFunction', aFunction);
+console.log('typeof of aFunction:', typeof aFunction);

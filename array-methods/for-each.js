b10b0e4751eedfaf4af0329660353f671644c204
sinadaryaei <sinatangssiran@gmail.com>
2023-06-20 19:00:51 +0000

    map.js completed.

--- array-methods/for-each.js
+++ array-methods/for-each.js
@@ -1,9 +1,11 @@
 const values = [10, 1, 22, 23, 41, 5, 18, 7, 80, 9];
 
+console.log('In order:');
 values.forEach((value) => {
-  console.log('In order:', value);
+  console.log(value);
 });
 
+console.log('Reverse order:');
 values.forEach((value, index, array) => {
-  console.log('Reverse order:', array[array.length - 1 - index]);
+  console.log(array[array.length - 1 - index]);
 });
--- array-methods/map.js
+++ array-methods/map.js
@@ -1,1 +1,12 @@
 const prices = [42.42, 10, 28.2234, 3.2, 5, 12];
+
+const priceObjects = prices.map((price) => ({
+  price,
+  salePrice: Number((price / 2).toFixed(4)),
+}));
+
+console.log('Price Objects:', priceObjects);
+
+const formattedPrices = prices.map((price) => `$${price.toFixed(2)}`);
+
+console.log('Formatted Prices:', formattedPrices);
